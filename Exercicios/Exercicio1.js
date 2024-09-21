2e87b1a77bf834f8b9099d9b7bb6bcf35212aa3f
vdallabona <vinicius.dallabona@gmail.com>
2024-09-20 21:49:59 -0300

    Exercicios até o 7

--- Exercicios/Exercicio1.js
+++ Exercicios/Exercicio1.js
@@ -18,4 +18,26 @@ function Exercicio4(temperatura){
     return resultado
 }
 
-module.exports = { Exercicio1, Exercicio2, Exercicio3, Exercicio4 }
+function Exercicio5(distancia){
+    const resultado = (distancia * 1.60934)
+    return resultado
+}
+
+function Exercicio6(num1){
+    var horas = Math.floor((num1 / 60)/60)
+    var minutos = Math.floor((num1 - horas * 3600) / 60)
+    var segundos = num1 - ((minutos * 60) + (horas * 3600))
+    var resultado = "O tempo convertido é de " + horas + " horas, " + minutos + " minutos e " + segundos + " segundos."
+    return resultado
+}
+
+function Exercicio7(num1){
+    var metros = Math.floor(num1 * 1000)
+    var centimetros = Math.floor(num1 * 100000) - metros * 100
+    var resultado = [];
+    resultado[1] = metros
+    resultado[2] = centimetros
+    return resultado
+}
+
+module.exports = { Exercicio1, Exercicio2, Exercicio3, Exercicio4, Exercicio5, Exercicio6, Exercicio7 }
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const Funcoes = require('./Exercicios/Exercicio1')
-
 const express = require('express')
 const app = express()
 const port = 3000
-
 app.use(express.json())
 
 app.post("/Exercicio1/", (req, res) => {
@@ -16,7 +14,6 @@ app.post("/Exercicio1/", (req, res) => {
 app.post('/exercicio2/', (req, res) => {
   const valor = req.body.valor
   const horas = req.body.horas
-
   const resultado = Funcoes.Exercicio2(valor, horas)
   res.status(201).json({ resultado: resultado })
 })
@@ -27,32 +24,35 @@ app.post('/exercicio3/', (req, res) => {
   const peso3 = req.body.peso3
   const peso4 = req.body.peso4
   const peso5 = req.body.peso5
-
   const resultado = Funcoes.Exercicio3(peso1, peso2, peso3, peso4, peso5)
   res.status(201).json({ resultado: resultado })
 })
 
 app.post('/exercicio4/', (req, res) => {
   const temperatura = req.body.temperatura
-  
   const resultado = Funcoes.Exercicio4(temperatura)
   res.status(201).json({ resultado: resultado })
 })
 
-app.get('/exercicio5/', (req, res) => {
-  const distancia = req.query.distancia
-  const resultado = (distancia * 1.60934)
-  res.json({ resultado })
+app.post('/exercicio5/', (req, res) => {
+  const distancia = req.body.distancia
+  const resultado = Funcoes.Exercicio5(distancia)
+  res.status(201).json({ resultado: resultado })
 })
 
 app.post("/exercicio6/", (req, res) => {
   const num1 = req.body.num1
+  const resultado = Funcoes.Exercicio6(num1)
+  res.status(201).json({ resultado: resultado })
+})
 
-  var horas = Math.floor((num1 / 60)/60)
-  var minutos = Math.floor((num1 - horas * 3600) / 60)
-  var segundos = num1 - ((minutos * 60) + (horas * 3600))
-
-  res.status(201).json({ horas: horas, minutos, segundos })
+app.post("/exercicio7/", (req, res) => {
+  const num1 = req.body.num1
+  const resultado = Funcoes.Exercicio7(num1)
+  res.status(201).json({
+    metros: resultado[1] ,
+    centimetros: resultado[2]
+  })
 })
 
 app.listen(port, () => {
